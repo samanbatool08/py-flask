42cb42339aa9ae26318259754dde64811814d4cb
Your Name <samanbatool08@gmail.com>
2020-05-02 13:09:36 -0400

    using user input

--- templates/application.py
+++ templates/application.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
 
+# Configure app
 app = Flask(__name__)
 
+# Registered students
+students = []
+
+
+# controllers
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -11,7 +17,8 @@ def register():
     name = request.form.get("name")
     dorm = request.form.get("dorm")
     if not name or not dorm:
-        return "failure"
+        return render_template("failure")
+    students.append(f"{name} from {dorm}")
     return render_template("success.html")
 
 
